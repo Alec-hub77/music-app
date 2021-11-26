d56114793ed7c8b013518503f93df4f1868767ae
Alec-hub77 <oleg.ostapiuk@yahoo.com>
2021-11-26 17:53:37 +0200

    track routes + upload files

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,16 @@
 import { Module } from "@nestjs/common";
+import { TrackModule } from './track/track.module';
+import  { FileModule } from './file/file.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-    
+    imports: [
+    MongooseModule.forRoot('mongodb+srv://alec:harley77@cluster0.ko5yx.mongodb.net/music-app?retryWrites=true&w=majority'),
+        TrackModule,
+        FileModule
+    ]
 })
+
 
 export class AppModule {
 
