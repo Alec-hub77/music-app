64bc2e36f21288b360ee537ae8889e01aaa31631
Alec-hub77 <oleg.ostapiuk@yahoo.com>
2021-11-30 16:40:57 +0200

    finished with routes

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,12 @@ import { Module } from "@nestjs/common";
 import { TrackModule } from './track/track.module';
 import  { FileModule } from './file/file.module';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ServeStaticModule } from "@nestjs/serve-static";
+import * as path from 'path';
 
 @Module({
     imports: [
+    ServeStaticModule.forRoot({rootPath: path.resolve(__dirname, 'static'),}),
     MongooseModule.forRoot('mongodb+srv://alec:harley77@cluster0.ko5yx.mongodb.net/music-app?retryWrites=true&w=majority'),
         TrackModule,
         FileModule
